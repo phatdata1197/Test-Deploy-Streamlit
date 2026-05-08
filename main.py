e4c0b3a90b467192c031c12952c888fd265a76d4
phatdata1197 <phat.data.1197@gmail.com>
2026-05-08 21:22:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import gspread as gs
+# import gspread as gs
 import pandas as pd
 import streamlit as st
 
-key_file = 'Google Sheet Connector.json'
-sheet_id = '1-YBba8d2RMhY5By3uT-NHSJ10Yt4ekcUwUb13kC3CmY'
-sheet_name = 'Trang tính1'
+# key_file = 'Google Sheet Connector.json'
+# sheet_id = '1-YBba8d2RMhY5By3uT-NHSJ10Yt4ekcUwUb13kC3CmY'
+# sheet_name = 'Trang tính1'
 
-client = gs.service_account(filename=key_file)
-sh = client.open_by_key(sheet_id)
-ws = sh.worksheet(sheet_name)
+# client = gs.service_account(filename=key_file)
+# sh = client.open_by_key(sheet_id)
+# ws = sh.worksheet(sheet_name)
 
-print(f"--- Kết nối thành công tới file: {sh.title} ---")
+# print(f"--- Kết nối thành công tới file: {sh.title} ---")
 
 lst = ['Name','Age','Job']
-# ws.append_row(lst)
-print(ws.get_all_values())
+# # ws.append_row(lst)
+# print(ws.get_all_values())
  
-data = ws.get_all_records()
-df = pd.DataFrame(data=data)
-df 
+# data = ws.get_all_records()
+# df = pd.DataFrame(data=data)
+# df 
 
 name = st.text_input("Nhap ten")
 age = st.number_input("Nhap tuoi", value= 18, step=1)
@@ -26,4 +26,6 @@ job = st.selectbox('Chon job',['DA','BI','DS'])
 row = [name,age,job]
 submit = st.button("Submit here")
 if submit:
-    ws.append_row(row)
+    st.success("Done")
+    st.balloons()
+    # ws.append_row(row)
